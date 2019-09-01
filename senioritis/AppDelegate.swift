32d6bc807209983ffb65a584cbb6e4304746b3d4
Sunghyun Cho <mail@chosunghyun.com>
2019-09-01 20:53:07 +0900

    Initial Commit

---
+++ senioritis/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  senioritis
+//
+//  Created by Sunghyun Cho on 01/09/2019.
+//  Copyright © 2019 Sunghyun Cho. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ senioritis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  senioritis
+//
+//  Created by Sunghyun Cho on 01/09/2019.
+//  Copyright © 2019 Sunghyun Cho. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
