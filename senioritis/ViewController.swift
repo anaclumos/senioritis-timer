9bdacc98aa1f6dbd19a07dee903b01ef356d9140
Sunghyun Cho <mail@chosunghyun.com>
2019-09-01 22:03:53 +0900

    [DEV] Finished Countdown MVP

--- senioritis/ViewController.swift
+++ senioritis/ViewController.swift
@@ -10,18 +10,98 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    @IBOutlet weak var timeField: NSTextField!
+    @IBOutlet weak var timeDisplay: NSTextField!
+    
+    var hour = 0
+    var minute = 0
+    var second = 0
+    var minuteString = ""
+    var secondString = ""
+    
+    var timer = Timer()
+    var timerON: Bool = false
+    
+    override func viewDidLoad() {super.viewDidLoad()}
+    override var representedObject: Any? {didSet {}}
+    
+    @IBAction func setButtonClicked(_ sender: Any) {
+        if !timerON {
+            timerON = true
+            let time = timeField.stringValue
+            formatSecondToTime(s: time)
+            displayTime()
+            runTimer()
         }
     }
-
-
+    
+    @IBAction func resetButtonClicked(_ sender: Any) {
+        endTimer()
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer (
+            timeInterval: 1,
+            target: self,
+            selector: #selector(ViewController.updateTimer),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc func updateTimer() {
+        print("\(hour):\(minute):\(second)")
+        reduceTime()
+        displayTime()
+    }
+    
+    func formatSecondToTime(s: String) {
+        let sec = Int(s)
+        hour = (sec!/3600)
+        minute = (sec!%3600)/60
+        second = (sec!%60)
+    }
+    
+    func reduceTime() {
+        if(second == 0){
+            if(minute == 0){
+                if(hour == 0){
+                    endTimer()
+                } else {
+                    hour -= 1
+                    minute = 59
+                    second = 60
+                }
+            } else {
+                minute -= 1
+                second = 60
+            }
+        }
+        second -= 1
+    }
+    
+    func displayTime() {
+        
+        if(minute < 10) {
+            minuteString = "0\(minute)"
+        } else {
+            minuteString = String(minute)
+        }
+        
+        if(second < 10) {
+            secondString = "0\(second)"
+        } else {
+            secondString = String(second)
+        }
+        
+        timeDisplay.stringValue = "\(hour):\(minuteString):\(secondString)"
+    }
+    
+    func endTimer() {
+        hour = 0; minute = 0; second = 0;
+        timer.invalidate()
+        timerON = false
+        displayTime()
+    }
 }
 
